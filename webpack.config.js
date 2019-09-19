f223fcf5d53d64e299d517349190f2b0cb8cf160
Alejandro Tabares Bosquet <alex311es@gmail.com>
2019-09-19 19:18:55 +0100

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  // devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  target: 'node',
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  }
+}
