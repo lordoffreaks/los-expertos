2429e3893b1dc7b014ee47f136a5c569b15c795e
Alejandro Tabares Bosquet <alex311es@gmail.com>
2019-09-26 07:56:56 +0100

    Add throng

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
+import throng from 'throng'
 import config from './config'
 import { createBot } from './bot'
 
 const logger = console
 const bot = createBot(config, logger)
 
-bot
-  .run()
-  .then(() => logger.info('App is up and running!'))
-  .catch(err => {
-    logger.error('Error starting the bot', err)
-  })
+throng((id: any) => {
+  logger.info(`Started worker ${id}`)
+  bot
+    .run()
+    .then(() => logger.info('App is up and running!'))
+    .catch(err => {
+      logger.error('Error starting the bot', err)
+    })
+})
